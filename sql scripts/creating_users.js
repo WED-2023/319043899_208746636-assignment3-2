2d10c79d7343d4a6a0d73f62f42f344aab85b967
Aviv Nachmias <avivnachmias23@gmail.com>
2025-05-15 12:20:23 +0300

    Creating user db and initial commit

---
+++ sql scripts/creating_users.js
@@ -0,0 +1,40 @@
+const mysql = require("mysql2/promise");
+require("dotenv").config({ path: "../.env" });
+(async () => {
+    console.log("Environment Variables:", {
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.DBpassword,
+        database: process.env.database,
+        port: process.env.port,
+      });
+  const connection = await mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.DBpassword,
+    database: process.env.database,
+    port: process.env.port,
+  });
+
+  try {
+    console.log("Starting the script...");
+    await connection.execute(`
+      CREATE TABLE IF NOT EXISTS users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        firstname VARCHAR(255) NOT NULL,
+        lastname VARCHAR(255) NOT NULL,
+        country VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        profilePic VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
+    console.log("Users table created successfully.");
+  } catch (error) {
+    console.error("Error creating tables:", error);
+  } finally {
+    await connection.end();
+  }
+})();
