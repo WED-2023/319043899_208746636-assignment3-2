c7ef86ad97cc2123ff146b4d653f2a00e549da2b
IdoBergerzon <idoberg@post.bgu.ac.il>
2025-05-17 10:17:25 +0300

    ...

--- sql_scripts/creating_recipes.js
+++ sql_scripts/creating_recipes.js
@@ -13,25 +13,26 @@ require("dotenv").config();
     user: process.env.user,
     password: process.env.DBpassword,
     database: process.env.database,
-    port: process.env.port,
+    port: process.env.DBport,
   });
 
   try {
     console.log("Starting the script...");
     await connection.execute(`
-      CREATE TABLE IF NOT EXISTS users (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) NOT NULL UNIQUE,
-        firstname VARCHAR(255) NOT NULL,
-        lastname VARCHAR(255) NOT NULL,
-        country VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        profilePic VARCHAR(255),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      CREATE TABLE IF NOT EXISTS recipes (
+        recipe_id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        picture VARCHAR(255) NOT NULL,
+        time VARCHAR(255) NOT NULL,
+        popularity VARCHAR(255) NOT NULL,
+        diet_type ENUM('vegan', 'vegetarian', 'none') NOT NULL,
+        gluten BOOLEAN NOT NULL,
+        created_by INT NOT NULL,
+        FOREIGN KEY (created_by) REFERENCES users(user_id),
+        description TEXT NOT NULL
       )
     `);
-    console.log("Users table created successfully.");
+    console.log("Recipes table created successfully.");
   } catch (error) {
     console.error("Error creating tables:", error);
   } finally {
---
+++ sql_scripts/creating_views.js
@@ -0,0 +1,37 @@
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+(async () => {
+    console.log("Environment Variables:", {
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.DBpassword,
+        database: process.env.database,
+        port: process.env.DBport,
+      });
+  const connection = await mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.DBpassword,
+    database: process.env.database,
+    port: process.env.DBport,
+  });
+
+  try {
+    console.log("Starting the script...");
+    await connection.execute(`
+        CREATE TABLE IF NOT EXISTS views (
+          view_id INT AUTO_INCREMENT PRIMARY KEY,
+          user_id INT NOT NULL,
+          recipe_id INT NOT NULL,
+          viewed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (user_id) REFERENCES users(user_id),
+          FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id)
+        )
+      `);
+    console.log("Recipes table created successfully.");
+  } catch (error) {
+    console.error("Error creating tables:", error);
+  } finally {
+    await connection.end();
+  }
+})();
